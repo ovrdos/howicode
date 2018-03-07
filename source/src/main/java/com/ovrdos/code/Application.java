f734a724da9e3c7cadba3c4a2b944b97ace7cb01
Kamal Hakim <kamal.hakim@nisr.com>
2018-03-07 13:35:29 -0700

    B. Testing for Anolomolies.

--- source/src/main/java/com/ovrdos/code/Application.java
+++ source/src/main/java/com/ovrdos/code/Application.java
@@ -5,19 +5,30 @@ package com.ovrdos.code;
 public class Application {
 
     private static final String CONFIG_DATA = "CONFIGURATION DATA FOR LOADING APPLICATION.";
+    private static final String CONFIG_OK = "CONFIG_OK";
+    private static final String CONFIG_ERROR = "CONFIG_ERROR";
+    private static final String APP_STARTED = " APP STARTED! ";
+    private static final String APP_NOT_STARTED = " APP NOT STARTED! ";
     private static boolean applicationStarted = false;
     private static boolean configLoaded = false;
 
 
-    public static void main(String arg[]) {
-        String config = "CONFIG_ERROR";
+    public static void main(String args[]) {
+        String config = CONFIG_ERROR;
+        if (args!=null) {
+            if (!(args[0]).equalsIgnoreCase(CONFIG_OK)) {
+                applicationStarted = false;
+                System.out.println(CONFIG_ERROR + APP_NOT_STARTED + config);
+                return;
+            }
+        }
         //This part of the application runs the initial
         // application startup and loads configuration.
         if (startApplication()) {
             config = loadConfigurations();
         }
 
-        System.out.println("APP STARTED! - " + config);
+        System.out.println(APP_STARTED + config);
     }
 
     private static boolean startApplication() {
--- source/src/test/java/com/ovrdos/code/ApplicationTest.java
+++ source/src/test/java/com/ovrdos/code/ApplicationTest.java
@@ -4,11 +4,9 @@ package com.ovrdos.code;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.mockito.internal.util.reflection.Whitebox;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -19,16 +17,25 @@ public class ApplicationTest {
     @Before
     public void setUp() {
         app = new Application();
-        app.main(null);
     }
 
     @Test
     public void given_App_Verify_App_Started() {
+        app.main(null);
         assertTrue("APPLICATION NOT STARTED!", (Boolean) Whitebox.getInternalState(app, "applicationStarted"));
     }
 
     @Test
     public void given_Config_Verify_Config_Loaded() {
+        app.main(null);
         assertTrue("CONFIG NOT LOADED!", (Boolean) Whitebox.getInternalState(app, "configLoaded"));
+    }
+
+    @Test
+    public void given_Bad_Config_Verify_App_Not_Loaded() {
+        String[]  configs = new String[1];
+        configs[0] = "CONFIG_ERROR";
+        app.main(configs);
+        assertFalse("CONFIG LOADED ANYWAY!", (Boolean) Whitebox.getInternalState(app, "configLoaded"));
     }
 }
